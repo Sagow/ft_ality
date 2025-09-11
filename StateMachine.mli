d3bafec51d1f92cfba34e3e557e98ca3db3f7468
Geoffrey Argence <argence.geoffrey@gmail.com>
2025-09-11 02:54:01 +0200

    FEAT [StateMachine]: An interface file was added for the StateMachine module

---
+++ StateMachine.mli
@@ -0,0 +1,11 @@
+type character_name = string
+type combo_name = string
+type combo_data = character_name * combo_name
+
+module TransitionEntryKV : (FtMap.KEYVALUE with type key_t = Utils.move and type value_t = (combo_data list * int))
+
+module TransitionMap : (FtMap.MAP with type key_t := TransitionEntryKV.key_t and type value_t := TransitionEntryKV.value_t)
+
+val build : Combo.Combo.t list -> TransitionMap.t list
+
+val perform_transition : TransitionMap.t list -> int -> Utils.move -> combo_data list * int
